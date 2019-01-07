a432c39b6f29c37515b8edd111c91f07af080a14
anitalmada <analaura.almada@gmail.com>
2019-01-06 22:55:12 -0300

    The trhee initial exercises

---
+++ Learning Python The Hard Way/ex3.py
@@ -0,0 +1,29 @@
+# Print a line telling what are you going to count
+print("I will now count my chickens.")
+
+# Print the result of the math you did to get de total numbers of hens and roosters
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# Print the result of the math I did to count the eggs
+print("Now I will count the eggs.")
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# I wonder if some expression is true or false and ask python to print the result
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
+
+# Ask python to do the math and print the result
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+# Print how I realize that the result of the boolean is false
+print("Oh, that's why it's False.")
+
+# I just can't get enough :P
+print("How about some more.")
+
+# Ask questions to python and print the answers right away
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
